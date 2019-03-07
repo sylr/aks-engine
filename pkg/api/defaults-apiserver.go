af77baaa1649c56ffdf124831fef7c460a44fbc1
Sylvain Rabot <s.rabot@lectra.com>
2019-03-07 10:29:15 +0100

    fix: move audit-policy from addons to manifests
    Signed-off-by: Sylvain Rabot <s.rabot@lectra.com>

--- pkg/api/defaults-apiserver.go
+++ pkg/api/defaults-apiserver.go
@@ -88,7 +88,7 @@ func (cs *ContainerService) setAPIServerConfig() {
 
 	// Audit Policy configuration
 	if common.IsKubernetesVersionGe(o.OrchestratorVersion, "1.8.0") {
-		defaultAPIServerConfig["--audit-policy-file"] = "/etc/kubernetes/addons/audit-policy.yaml"
+		defaultAPIServerConfig["--audit-policy-file"] = "/etc/kubernetes/manifests/audit-policy.yaml"
 	}
 
 	// RBAC configuration
